2978c55e8fb74efe70bbf0e8b916feb87de38b46
Michael Timothy <michael@michaelgrannan.com>
2019-04-03 20:59:52 -0500

    Function Based Components
    Reuse JSX Elements with Function based Components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 import "./styles.css";
 
 const App = () => {
-  const buttonText = "Click Me";
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter Name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: "black", color: "white" }}>
-        {buttonText}
-      </button>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <h1>Warning!</h1>
+        Are you sure you want to do that?
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jake"
+          timeAgo="Today at 4:45pm"
+          comment="Seriously sick kicks man!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Justin"
+          timeAgo="Today at 2:00am"
+          comment="Off the wall"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jim"
+          timeAgo="Yesterday at 1:00pm"
+          comment="Jeepers! Where'd you get those sneakers?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
